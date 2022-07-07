2137fe08f2d53b14d82681ef6b95c1406bd53a6e
orochan <108882503+orochan@users.noreply.github.com>
2022-07-07 23:33:18 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// /**
+//  *  const,let等の変数宣言
+//  */
+// var val1 = "var変数";
+// // console.log(val1);
+
+// // val1 = "varvaar";
+// // console.log(val1);
+
+// // // var変数は再宣言可能
+// // var val1 ="grehpaassadadadahsjpg"
+// // console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = val1;
+// console.log(val2);
+
+const val3 = "val";
+console.log(val3);
+
+const val3 = "aaa";
